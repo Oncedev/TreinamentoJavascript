6371fc2fa7b4ba0819f35a61df077c078ee90db8
pauloortins <paulo.ortins@gmail.com>
2014-03-18 18:34:00 -0300

    Adicionado exemplos de Math

---
+++ 2-Math/exemplo.js
@@ -0,0 +1,12 @@
+console.debug(Math.pow(2,53));   // => 9007199254740992: 2 elevando a 53
+console.debug(Math.round(.6));   // => 1.0: arredondar para o inteiro mais próximo
+console.debug(Math.ceil(.6));    // => 1.0: arredondar para cima
+console.debug(Math.floor(.6));   // => 0.0: arredondar para baixo
+console.debug(Math.abs(-5));     // => 5: retornar o valor absoluto
+console.debug(Math.max(5,6,7));  // Retorna o maior valor
+console.debug(Math.min(5,6,7));  // Retorna o menor valor
+console.debug(Math.random());    // Retorna um número randomico entre 0 e 1.0
+console.debug(Math.sqrt(3));     // Retorna a raiz de 3
+console.debug(Math.pow(3, 1/3)); // The cube root of 3
+console.debug(Math.sin(0));      // Calcula o seno
+console.debug(Math.log(10));     // Log de 10
